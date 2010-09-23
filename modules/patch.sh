e15639c5577f818fc3e26e8300182ce50d9cd358
Michał Górny <gentoo@mgorny.alt.pl>
2010-09-23 23:54:19 +0200

    Support running sed on files using patch module.

---
+++ modules/patch.sh
@@ -0,0 +1,25 @@
+#!/bin/false
+# fastconf -- file manipulations (patching) support
+# (c) 2010 Michał Górny
+# Released under the terms of the 3-clause BSD license.
+
+# Synopsis: fc_sed <in> <out> <sed-command> [...]
+fc_sed() {
+	local in out ec
+	in=${1}
+	out=${2}
+	shift
+	shift
+
+	# Prepend each sed command with '-e' and quote it.
+	ec=$(( ${#} * 2 ))
+	while [ ${#} -lt ${ec} ]; do
+		set -- "${@}" -e "\"${1}\""
+		shift
+	done
+
+	printf '%s: %s\n\tsed %s $< > $@' \
+		"${out}" "${in}" "${*}"
+
+	_fc_append_output "${out}"
+}
