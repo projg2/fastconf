dc54cdfc6a921e2c9dc9a01d161600e18a70f3e7
Michał Górny <gentoo@mgorny.alt.pl>
2010-09-25 19:07:26 +0200

    Support wrapping around pkg-config through fc_pkg_config().

--- modules/cc_common.sh
+++ modules/cc_common.sh
@@ -5,10 +5,43 @@
 
 fc_mod_cc_common_init() {
 	fc_export_functions \
+		fc_mod_cc_common_cmdline_parsed \
 		fc_mod_cc_common_get_targets
+}
+
+fc_mod_cc_common_cmdline_parsed() {
+	unset PKGCONFIG_WARNED
 }
 
 fc_mod_cc_common_get_targets() {
 	fc_export CPPFLAGS "${CPPFLAGS}"
 	fc_export LDFLAGS "${LDFLAGS}"
+}
+
+# Synopsis: fc_pkg_config [...]
+# Tries to find ${CHOST}-pkg-config or pkg-config, and calls it with
+# supplied arguments. If it is unable to find any pkg-config, it outputs
+# a warning and returns 127.
+fc_pkg_config() {
+	if [ -z "${PKGCONFIG}" ]; then
+		if [ -z "${PKGCONFIG_WARNED}" ]; then
+			if [ -n "${CHOST}" ] && fc_have "${CHOST}"-pkg-config; then
+				PKGCONFIG=${CHOST}-pkg-config
+			elif fc_have pkg-config; then
+				PKGCONFIG=pkg-config
+
+				if [ "${CBUILD}" != "${CHOST}" ]; then
+					echo "WARNING: cross-compiling but no ${CHOST}-pkg-config found." >&2
+				fi
+			else
+				echo "WARNING: pkg-config not found." >&2
+				PKGCONFIG_WARNED=1
+				return 127
+			fi
+		else
+			return 127
+		fi
+	fi
+
+	"${PKGCONFIG}" "${@}"
 }
